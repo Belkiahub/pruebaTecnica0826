b76442181e3079aad91f16ed371d3cab028e3250
KevinSilva <kevinsilvadb503@gmail.com>
2024-08-26 08:24:00 -0600

    servicios y repositorio de categoriav

---
+++ src/main/java/com/example/PruebaTecnica0826/servicio/implementacion/CategoriaVService.java
@@ -0,0 +1,36 @@
+package com.example.PruebaTecnica0826.servicio.implementacion;
+
+
+import com.example.PruebaTecnica0826.modelos.CategoriaV;
+import com.example.PruebaTecnica0826.repositorio.ICategoriaVRepository;
+import com.example.PruebaTecnica0826.servicio.interfaces.ICategoriaVService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class CategoriaVService implements ICategoriaVService {
+    @Autowired
+    private ICategoriaVRepository categoriaVRepository;
+
+    @Override
+    public List<CategoriaV> obtenerTodos() {
+        return categoriaVRepository.findAll();
+    }
+
+    @Override
+    public Optional<CategoriaV> buscarPorId(Long id) {
+        return categoriaVRepository.findById(id);
+    }
+
+    @Override
+    public CategoriaV crearOEditar(CategoriaV categoriaV) {
+        return categoriaVRepository.save(categoriaV);
+    }
+
+    @Override
+    public void eliminarPorId(Long id) {
+        categoriaVRepository.deleteById(id);
+    }
+}
---
+++ src/main/java/com/example/PruebaTecnica0826/servicio/interfaces/ICategoriaVService.java
@@ -0,0 +1,20 @@
+package com.example.PruebaTecnica0826.servicio.interfaces;
+
+import com.example.PruebaTecnica0826.modelos.CategoriaV;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ICategoriaVService {
+
+    List<CategoriaV> obtenerTodos();
+
+
+    Optional<CategoriaV> buscarPorId(Long id);
+
+    CategoriaV crearOEditar(CategoriaV categoriaV);
+
+    void eliminarPorId(Long id);
+
+
+}
